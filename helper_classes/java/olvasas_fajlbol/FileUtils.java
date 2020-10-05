5d148b2ff21895bfb5cfad28c70c32319e3d1346
Jabba Laci <jabba.laci@gmail.com>
2020-10-05 09:05:56 +0200

    FileUtils.java added

---
+++ helper_classes/java/olvasas_fajlbol/FileUtils.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils
+{
+    // input: a megnyitandó file neve
+    // output: egy List<String> objektum, ami az egyes sorokat tartalmazza
+    public static List<String> readLines(String fileName)
+    {
+        List<String> lines = new ArrayList<String>();
+
+        try
+        {
+            String line;
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null)
+            {
+                lines.add(line);
+            }
+            br.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            System.exit(2);
+        }
+
+        return lines;
+    }
+}
